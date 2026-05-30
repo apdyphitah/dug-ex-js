b7e39533fa06b2418f9db468d7cdd153c2949be7
dige <maqdashaawi@gmail.com>
2026-05-30 14:25:51 +0300

    solution1

---
+++ exercise1/ex1.js
@@ -0,0 +1,13 @@
+let x = 7
+let y = 3
+
+let sum = x + y
+console.log(sum)
+let difference = x - y
+console.log(difference)
+let product = x * y
+console.log(product)
+let quotient = x / y
+console.log(quotient)
+let remainder = x % y
+console.log(remainder)
