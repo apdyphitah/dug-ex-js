2754ea18eff2cc3f9b3c293e04dec9ddb66c0dde
dige <maqdashaawi@gmail.com>
2026-05-30 17:05:35 +0300

    ex7

---
+++ exercise7/index.js
@@ -0,0 +1,13 @@
+let colurs = ["red", "green", "blue"]
+
+console.log(colurs) // red
+
+colurs.push("yellow")
+
+//adding yellow to the end of the array
+
+console.log(colurs) // red, green, blue, yellow
+
+//removing the first element of the array
+colurs.shift()
+console.log(colurs) // green, blue, yellow
