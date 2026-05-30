701a8fc1577d1a394661b1ad7be3804fa1594d21
dige <maqdashaawi@gmail.com>
2026-05-30 14:53:36 +0300

    ex2

---
+++ exercise2/index.js
@@ -0,0 +1,19 @@
+let a = 15
+let b = "15"
+
+// equal to operator (==) checks for value equality
+console.log(a == b) // true
+
+console.log(a != b) // true
+// strict equal to operator (===) checks for both value and type equality
+console.log(a === b) // false
+console.log(a !== b) // true
+
+// greater than operator (>)
+console.log(a > b) // false
+// less than operator (<)
+console.log(a < b) // false     
+// greater than or equal to operator (>=)
+console.log(a >= b) // true
+// less than or equal to operator (<=)
+console.log(a <= b) // true
