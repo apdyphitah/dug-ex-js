a2aeb4eadfb1c7f35b177ca119fdab4d4dcfe2db
dige <maqdashaawi@gmail.com>
2026-05-31 13:36:05 +0300

    ex9

---
+++ exercise9/index.js
@@ -0,0 +1,21 @@
+let car = {
+
+    make: "Toyota",
+
+    model: "Corolla",
+
+    year: 2020,
+
+    start: function() {
+
+        console.log("The car has started.");
+
+}
+}
+
+console.log(car.start());
+
+
+
+
+
