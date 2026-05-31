041e16fae99f79199d481dccf5a20b5ad161548c
dige <maqdashaawi@gmail.com>
2026-05-31 14:02:55 +0300

    ex10

---
+++ exercise10/index.js
@@ -0,0 +1,21 @@
+let libarary = [
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        year: 1925
+    },
+    {
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        year: 1960
+    },
+    {
+        title: "1984",
+        author: "George Orwell",
+        year: 1948
+    }
+
+]
+
+console.log(libarary[0].title);
+console.log(libarary[1].author);
