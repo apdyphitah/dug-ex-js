b5e7abee0ce792550c227f06e5fe9f898cf013e7
dige <maqdashaawi@gmail.com>
2026-06-01 15:14:52 +0300

    ex3

---
+++ exercise33/index.js
@@ -0,0 +1,9 @@
+const header = document.querySelector("#header");
+const paragraph = document.querySelector("#paragraph");
+
+function changeContent() {
+    header.textContent = "thanks for clicking!";
+    paragraph.innerHTML = "you clicked the button!";
+}
+
+
