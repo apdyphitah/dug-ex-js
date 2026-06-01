b7f3b08c4f4b178b0419204e950e69cb44398e9c
dige <maqdashaawi@gmail.com>
2026-06-01 14:27:02 +0300

    ex31

---
+++ exercise31/index.js
@@ -0,0 +1,16 @@
+async function data() {
+    try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/users")
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }else {
+            const data = await response.json();
+            console.log(data);
+        }
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
+
+data();
